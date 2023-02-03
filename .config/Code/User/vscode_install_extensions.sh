d7333aef562e1934b987840a4d2458f7e455524c
kiyop <tequenica@gmail.com>
2023-02-03 13:30:03 +0900

    update vs-code settings

--- .config/Code/User/vscode_install_extensions.sh
+++ .config/Code/User/vscode_install_extensions.sh
@@ -2,49 +2,44 @@
 
 # 使用中の拡張
 VSCODE_EXTLIST=(
-    # 【汎用】スタイル・見栄え関連
-    bceskavich.theme-dracula-at-night
-    PKief.material-icon-theme
-    # 【汎用】エディター関連
-    eamodio.gitlens
-    wmaurer.change-case
-    shardulm94.trailing-spaces
-    EditorConfig.EditorConfig
-    mikestead.dotenv
-    eriklynd.json-tools
-    cssho.vscode-svgviewer
-    # 【汎用】開発・デバッガー関連
-    ms-azuretools.vscode-docker
-    msjsdiag.debugger-for-chrome
-    formulahendry.code-runner
-    # 言語別の拡張
-    felixfbecker.php-debug
-    felixfbecker.php-intellisense
-    eg2.vscode-npm-script
-    dbaeumer.vscode-eslint
-    esbenp.prettier-vscode
-    octref.vetur
+  # 【汎用】スタイル・見栄え関連
+  bceskavich.theme-dracula-at-night
+  PKief.material-icon-theme
+  # 【汎用】エディター関連
+  eamodio.gitlens
+  wmaurer.change-case
+  shardulm94.trailing-spaces
+  EditorConfig.EditorConfig
+  mikestead.dotenv
+  eriklynd.json-tools
+  vitaliymaz.vscode-svg-previewer
+  # 【汎用】開発・デバッガー関連
+  ms-azuretools.vscode-docker
+  # ms-vscode.remote-explorer
+  # ms-vscode-remote.remote-containers
+  # ms-vscode-remote.remote-ssh
+  # ms-vscode-remote.remote-ssh-edit
+  # ms-vscode-remote.remote-wsl
+  # ms-vscode-remote.vscode-remote-extensionpack
+  # 言語別の拡張
+  xdebug.php-debug
+  zobo.php-intellisense
+  dbaeumer.vscode-eslint
+  esbenp.prettier-vscode
+  octref.vetur
+  svelte.svelte-vscode
+  JuanBlanco.solidity
 )
 
 # 未使用だけどインストールしておくもの
 VSCODE_EXTLIST_UNUSED=(
-    Tyriar.sort-lines
-    patrys.vscode-code-outline
-    jebbs.plantuml
-    ritwickdey.LiveServer
-    bungcip.better-toml
-    karunamurti.haml
-    golang.go
-    rust-lang.rust
-    Dart-Code.dart-code
-    Dart-Code.flutter
-    JuanBlanco.solidity
+  ms-vscode.live-server
 )
 
 for e in ${VSCODE_EXTLIST[@]}; do
-    code --install-extension $e
+  code --install-extension $e
 done
 for e in ${VSCODE_EXTLIST_UNUSED[@]}; do
-    code --install-extension $e
-    code --disable-extension $e
+  code --install-extension $e
+  code --disable-extension $e
 done
